07ec278e085811f5f5bc260555dc97ff33a71ed0
M Samuel Vijaykumar <samof76@gmail.com>
2022-04-15 21:50:52 +0530

    Customize defaults

--- ngx/__main__.py
+++ ngx/__main__.py
@@ -3,19 +3,25 @@
 import pulumi
 from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
 from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
-from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs
+from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs, Namespace
+
+config = pulumi.Config()
+ns_name = config.get('namespace')
+replicas = config.get_int('replicas') or 1
 
 app_labels = { "app": "nginx" }
-
+namespace = Namespace("web", metadata = ObjectMetaArgs(name=ns_name))
 deployment = Deployment(
     "nginx",
+    metadata = ObjectMetaArgs(name="nginx", namespace=namespace.metadata["name"]),
     spec=DeploymentSpecArgs(
         selector=LabelSelectorArgs(match_labels=app_labels),
-        replicas=1,
+        replicas=replicas,
         template=PodTemplateSpecArgs(
             metadata=ObjectMetaArgs(labels=app_labels),
             spec=PodSpecArgs(containers=[ContainerArgs(name="nginx", image="nginx")])
         ),
     ))
 
-pulumi.export("name", deployment.metadata["name"])
+pulumi.export("namespace_name", namespace.metadata["name"])
+pulumi.export("deployment_name", deployment.metadata["name"])
